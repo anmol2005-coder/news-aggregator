706377ed29934375a32055883319b945160104df
anmol <gadadeanmol@gmail.com>
2026-04-09 10:41:45 +0530

    fixed backend api url

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api/auth";
+const BASE_URL = "https://news-aggregator-mxze.onrender.com/api/auth";
 
 // LOGIN
 export const loginUser = async (formData) => {
